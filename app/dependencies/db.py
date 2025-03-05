491e82be61e69f43c3a06f2b0f850bbbd9e1bf43
Erzhan <erzhanmuratov99@gmail.com>
2025-03-05 14:42:51 +0600

    feat: add db and user deps

---
+++ app/dependencies/db.py
@@ -0,0 +1,13 @@
+from typing import Annotated
+from collections.abc import Generator
+
+from fastapi import Depends
+from sqlmodel import Session
+from core.db import engine
+
+def get_db() -> Generator[Session, None, None]:
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_db)]
