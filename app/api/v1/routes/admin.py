6f90b1b164fc684038c1e6ac1fa022e1e224305c
Erzhan <erzhanmuratov99@gmail.com>
2025-03-07 18:19:30 +0600

    feat: add admin create user route

--- app/api/v1/routes/admin.py
+++ app/api/v1/routes/admin.py
@@ -1,12 +1,14 @@
 from typing import Any
 
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException, status
 from sqlmodel import select, func
 
 from app.models.user import User, UserPublic, UsersPublic, UserCreate
-from crud import user as user_crud
+from app.crud import user as user_crud
+from app.core.config import settings
 from app.dependencies.db import SessionDep
 from app.dependencies.user import CurrentSuperUser
+from app.utils.email import generate_new_account_email, send_email
 
 router = APIRouter(tags=["admin"])
 
@@ -23,11 +25,25 @@ async def read_users(session: SessionDep, skip: int = 0, limit: int = 100) -> An
 
 @router.post("/users", dependencies=CurrentSuperUser, response_model=UserPublic)
 async def create_user(*, session: SessionDep, user_in: UserCreate) -> UserPublic:
-	# user = user_crud.get_user_by_email(session=session, email=user_in.email)
-	# if not user:
-	# 	raise
-	pass
+	user = user_crud.get_user_by_email(session=session, email=user_in.email)
+	if user:
+		raise HTTPException(
+			status_code=status.HTTP_400_BAD_REQUEST,
+			detail="The user with this email already exists in the system."
+		)
+	user = user_crud.create_user(session=session, user_create=user_in)
+	if settings.emails_enabled and user_in.email:
+		email_data = generate_new_account_email(
+			email_to=user_in.email, username=user_in.email, password=user_in.password
+		)
+		send_email(
+			email_to=user_in.email,
+			subject=email_data.subject,
+			html_content=email_data.html_content
+		)
 
+	return user
+	
 
 
 	
--- app/utils/email.py
+++ app/utils/email.py
@@ -93,3 +93,21 @@ def verify_password_reset_token(token: str) -> str | None:
         return str(decoded_token["sub"])
     except InvalidTokenError:
         return None
+    
+
+def generate_new_account_email(
+    email_to: str, username: str, password: str
+) -> EmailData:
+    project_name = settings.PROJECT_NAME
+    subject = f"{project_name} - New account for user {username}"
+    html_content = render_email_template(
+        template_name="new_account.html",
+        context={
+            "project_name": settings.PROJECT_NAME,
+            "username": username,
+            "password": password,
+            "email": email_to,
+            "link": settings.FRONTEND_HOST,
+        },
+    )
+    return EmailData(html_content=html_content, subject=subject)
